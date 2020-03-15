f6c39dda105b9b5ceb38a69b29e1998230e845a9
kimiinglet <kinglet91391@gmail.com>
2020-03-15 16:21:49 -0500

    api key/ search fetch

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-//API KEY
-var APIKey = "69f15c00682d18316696a7296341bf67";
+// appId
+let APIKey = '69f15c00682d18316696a7296341bf67';
+let units = 'imperial'
+let searchMethod = 'zip';
 
+function searchWeather(searchTerm) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${APIKey}&units=${units}`).then(result => {
+        return result.json();
+    }).then(result => {
+        init(result);
+    })
+}
+
+function init(resultFromServer) {
+    console.log(resultFromServer)
+}
+
+document.getElementById('searchBtn').addEventListener('click', () => {
+    let searchTerm = document.getElementById('searchInput').value;
+    if(searchTerm)
+        searchWeather(searchTerm);
+})
