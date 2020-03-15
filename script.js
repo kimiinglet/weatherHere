54baa85008d9202931274cc1c29d962257229624
kimiinglet <kinglet91391@gmail.com>
2020-03-15 16:37:26 -0500

    image on weather

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let APIKey = '69f15c00682d18316696a7296341bf67';
 let units = 'imperial'
 let searchMethod;
 
-// City or Zip
+// City or Zip function
 function getSearchMethod(searchTerm) {
     if(searchTerm.length === 5 && Number.parseInt(searchTerm) + '' === searchTerm) 
     searchMethod = 'zip';
@@ -23,7 +23,32 @@ function searchWeather(searchTerm) {
 }
 
 function init(resultFromServer) {
-    console.log(resultFromServer)
+    //background based on weather
+    //case sensitive
+    switch (resultFromServer.weather[0].main) {
+        case 'Clear':
+            document.body.style.backgroundImage = 'url("assets/images/clear.jpg")'
+            break;
+
+        case 'Clouds':
+            document.body.style.backgroundImage = 'url("assets/images/cloudy.jpg")'
+            break;
+
+        case 'Rain':
+        case 'Drizzle':
+        case 'Mist':
+            document.body.style.backgroundImage = 'url("assets/images/rain.jpg")'
+            break;
+
+        case 'Thunderstorm':
+            document.body.style.backgroundImage = 'url("assets/images/storm.jpg")'
+            break;
+
+        case 'Snow':
+            document.body.style.backgroundImage = 'url("assets/images/snow.jpg")'
+
+            break;
+    }
 }
 
 document.getElementById('searchBtn').addEventListener('click', () => {
