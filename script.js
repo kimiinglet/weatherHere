3f940121ab50a9a0134a051928295bf8ffea63a6
kimiinglet <kinglet91391@gmail.com>
2020-03-15 16:28:22 -0500

    search city and zip

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 // appId
 let APIKey = '69f15c00682d18316696a7296341bf67';
 let units = 'imperial'
-let searchMethod = 'zip';
+let searchMethod;
 
+// City or Zip
+function getSearchMethod(searchTerm) {
+    if(searchTerm.length === 5 && Number.parseInt(searchTerm) + '' === searchTerm) 
+    searchMethod = 'zip';
+    else 
+    searchMethod = 'q';
+    
+}
+
+// API information on search
 function searchWeather(searchTerm) {
+    getSearchMethod(searchTerm)
     fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${APIKey}&units=${units}`).then(result => {
         return result.json();
     }).then(result => {
